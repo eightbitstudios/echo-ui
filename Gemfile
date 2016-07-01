8935f2512ceadde346ba638f161130a7d54d1f95
Brian Toombs <bp.toombs@gmail.com>
2016-07-01 14:59:24 -0500

    adding Echo Rep and Carrier Rep template files. include Build files and Source files. Build created using Middleman.

---
+++ Gemfile
@@ -0,0 +1,26 @@
+# If you do not have OpenSSL installed, change
+# the following line to use 'http://'
+source 'https://rubygems.org'
+
+
+# Middleman Gems
+###############################
+gem 'middleman'
+gem 'middleman-livereload'
+gem 'middleman-compass', '>= 4.0.1'
+gem 'middleman-sprockets', '>= 4.0.0'
+gem 'middleman-autoprefixer'
+# gem 'middleman-deploy'
+
+# Middleman Extensions
+###############################
+# (Middleman Deploy (https://github.com/middleman-contrib/middleman-deploy/pull/87))
+gem 'middleman-deploy', git: 'https://github.com/middleman-contrib/middleman-deploy', branch: 'master'
+
+# Frameworks/Library
+###############################
+# Bootstrap
+gem 'bootstrap-sass', '>= 3.3.6'
+
+# Font Awesome
+# gem 'font-awesome-sass'
---
+++ config.rb
@@ -0,0 +1,112 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+page "/shell-echorep.html", layout: :layout_echorep
+
+page "/login.html", layout: :no_headerfooter
+page "/password.html", layout: :no_headerfooter
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+# which_fake_page: "Rendering a fake page with a local variable" }
+
+###
+# Helpers
+###
+
+activate :autoprefixer
+
+###
+# Environment List
+###
+
+# Server Environment
+configure :server do
+
+  # Debug assets
+  set :debug_assets, true
+
+end
+
+# Development Environment
+configure :development do
+
+  # Automatic image dimensions on image_tag helpers
+  activate :automatic_image_sizes
+
+  # Reload the browser automatically whenever files change
+  activate :livereload,  :no_swf => true
+
+  # Assets Pipeline Sets
+  set :haml, {ugly: false, format: :html5}
+  set :css_dir, 'assets/stylesheets'
+  set :js_dir, 'assets/javascripts'
+  set :images_dir, 'assets/images'
+  set :fonts_dir, 'assets/fonts'
+
+  # Pretty URLs
+  # activate :directory_indexes
+
+end
+
+# Build Environment
+configure :build do
+
+  # Minify CSS on build
+  # activate :minify_css
+
+  # Minify Javascript on build
+  # activate :minify_javascript
+
+  # GZIP text files
+  # activate :gzip
+
+  # Use relative URLs
+  activate :relative_assets
+
+  # Middleman Deploy (https://github.com/middleman-contrib/middleman-deploy/)
+  # activate :deploy do |deploy|
+  #  deploy.deploy_method = :git
+  #  Optional Settings
+  #  deploy.remote   = 'https://github.com/coskuntekin/baidu_weather_widget.git' # remote name or git url, default: origin
+  #  deploy.branch   = 'gh-pages' # default: gh-pages
+  #  deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
+  #  deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+  # end
+
+end
+
+
+activate :deploy do |deploy|
+  deploy.deploy_method  = :ftp
+  deploy.port           = 22
+  deploy.host           = "8bitstudios.com"
+  deploy.path           = "/clients/echo"
+  deploy.user           = "u37443755"
+  deploy.password       = "OhNyes!11"
+end
+
+
+
+# Production Environment
+configure :production do
+
+  # Assets Pipeline Sets
+  set :css_dir, 'assets/stylesheets'
+  set :js_dir, 'assets/javascripts'
+  set :images_dir, 'assets/images'
+  set :fonts_dir, 'assets/fonts'
+
+  # Middleman Production dev server run code
+  # 'middleman server -e production'
+
+end
