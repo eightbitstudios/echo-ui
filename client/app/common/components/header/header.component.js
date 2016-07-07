307521f0ad6c418943e17ff46a3bd146115555ea
Milan Preradovic <milanp@slalom.com>
2016-07-07 09:50:33 -0500

    BAT-463: added carrier nav

--- client/app/common/components/header/header.component.js
+++ client/app/common/components/header/header.component.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('echo.components.header', [
-  'echo.components.eyeBrowNav'
+  'echo.components.eyeBrowNav',
+  'echo.components.carrierNav'
 ]).component('appHeader', {
   templateUrl: 'app/common/components/header/header.template.html'
 });
