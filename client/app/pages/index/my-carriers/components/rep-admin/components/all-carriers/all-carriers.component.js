dc92cd244cc6fc61e171b7b645728d3d2086fc94
Milan Preradovic <milanp@slalom.com>
2016-07-08 12:42:21 -0500

    BAT-437: added all carriers component

---
+++ client/app/pages/index/my-carriers/components/rep-admin/components/all-carriers/all-carriers.component.js
@@ -0,0 +1,5 @@
+angular.module('echo.index.myCarriers.repAdmin.allCarriers', [])
+.component('allCarriers', {
+  templateUrl: 'app/pages/index/my-carriers/components/rep-admin/components/all-carriers/all-carriers.template.html',
+  bindings: {}
+});
--- client/app/pages/index/my-carriers/components/rep-admin/rep-admin.component.js
+++ client/app/pages/index/my-carriers/components/rep-admin/rep-admin.component.js
@@ -1,8 +1,7 @@
 angular.module('echo.index.myCarriers.repAdmin', [
-  'echo.components.navbar'
+  'echo.components.navbar',
+  'echo.index.myCarriers.repAdmin.allCarriers'
 ]).component('repAdmin', {
   templateUrl: 'app/pages/index/my-carriers/components/rep-admin/rep-admin.template.html',
-  bindings: {
-    carrierDetails: '='
-  }
+  bindings: {}
 });
