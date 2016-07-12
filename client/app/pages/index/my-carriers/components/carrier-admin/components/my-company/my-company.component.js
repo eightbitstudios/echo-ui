e7db19695480dd3a12a9916832542374569d0404
Milan Preradovic <milanp@slalom.com>
2016-07-12 16:17:59 -0500

    BAT-429: added portals page

--- client/app/pages/index/my-carriers/components/carrier-admin/components/my-company/my-company.component.js
+++ client/app/pages/index/my-carriers/components/carrier-admin/components/my-company/my-company.component.js
@@ -1,5 +1,16 @@
-angular.module('echo.index.myCarriers.carrierAdmin.myCompany', [])
-.component('myCompany', {
-  templateUrl: 'app/pages/index/my-carriers/components/carrier-admin/components/my-company/my-company.template.html',
-  bindings: {}
-});
+angular.module('echo.index.myCarriers.carrierAdmin.myCompany', [
+  'echo.components.portalUsers',
+  'echo.services.carrier',
+  'echo.services.carrierDetails'
+])
+  .component('myCompany', {
+    templateUrl: 'app/pages/index/my-carriers/components/carrier-admin/components/my-company/my-company.template.html',
+    bindings: {},
+    controller: function (carrierService, carrierDetailsService) {
+      var that = this;
+      that.carrier = carrierDetailsService.getCarrierDetails();
+      carrierService.fetchCarrierPortalUsers(that.carrier.id).then(function (portalUsers) {
+        that.portalUsers = portalUsers;
+      });
+    }
+  });
