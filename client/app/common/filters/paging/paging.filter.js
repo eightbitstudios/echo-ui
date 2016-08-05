d98af0a9269f36e8d3faeb55463a49d70b87a5e6
Milan Preradovic <milanp@slalom.com>
2016-08-05 15:37:38 -0500

    Fixed joshing error

--- client/app/common/filters/paging/paging.filter.js
+++ client/app/common/filters/paging/paging.filter.js
@@ -4,7 +4,7 @@ angular.module('echo.filters.paging', [])
   .filter('paging', function () {
     return function (pagingModel, recordType) {
       var lastRecord = pagingModel.offset + pagingModel.recordCount - 1;
-      var template = _.template('Showing ${firstRecord}-${lastRecord} of ${totalRecords} ${recordType}')
+      var template = _.template('Showing ${firstRecord}-${lastRecord} of ${totalRecords} ${recordType}');
       return template({
         firstRecord: pagingModel.offset,
         lastRecord: lastRecord,
