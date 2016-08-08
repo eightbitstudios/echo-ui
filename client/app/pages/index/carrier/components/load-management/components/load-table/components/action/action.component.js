3e90e38282d9f51ac7e85c146205c53a773d2a04
Milan Preradovic <milanp@slalom.com>
2016-08-08 09:51:39 -0500

    Remove filter

--- client/app/pages/index/carrier/components/load-management/components/load-table/components/action/action.component.js
+++ client/app/pages/index/carrier/components/load-management/components/load-table/components/action/action.component.js
@@ -1,7 +1,6 @@
 angular.module('echo.index.carrier.loadManagement.loadTable.action', [
   'echo.filters.firstCharacter',
-  'echo.config.appConstants',
-  'echo.filters.reportDate'
+  'echo.config.appConstants'
 ])
   .component('action', {
     templateUrl: 'app/pages/index/carrier/components/load-management/components/load-table/components/action/action.template.html',
