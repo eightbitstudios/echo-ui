463934e189d2f2d56d13af8a9cf6fddfae3586b1
Milan Preradovic <milanp@slalom.com>
2016-08-09 13:02:00 -0500

    Added warning icon and details updated css and html

--- client/app/pages/index/carrier/components/load-management/components/load-table/components/load/load.component.js
+++ client/app/pages/index/carrier/components/load-management/components/load-table/components/load/load.component.js
@@ -3,7 +3,8 @@ angular.module('echo.index.carrier.loadManagement.loadTable.load', [])
     templateUrl: 'app/pages/index/carrier/components/load-management/components/load-table/components/load/load.template.html',
     bindings: {
       loadNumber: '<',
-      proNumber: '<'
+      proNumber: '<',
+      isDetailsUpdated: '<'
     },
     controller: function () {}
   });
