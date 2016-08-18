5883627d18fbe58699a7df3340b062935c92e9f4
Milan Preradovic <milanp@slalom.com>
2016-08-18 15:33:42 -0500

    BAT-121: List items component

--- client/app/common/components/collapsable/collapsable.component.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('echo.components.collapsable', []).component('collapsable', {
-  bindings: {},
-  templateUrl: 'app/common/components/collapsable/collapsable.template.html',
-  controller: function() { 
-  }
-});
--- client/app/common/components/item-list/item-list.component.js
+++ client/app/common/components/item-list/item-list.component.js
@@ -4,5 +4,8 @@ angular.module('echo.components.itemList', []).component('itemList', {
   bindings: {},
   templateUrl: 'app/common/components/item-list/item-list.template.html',
   controller: function() { 
+    var that = this;
+
+    that.isOpen = false;
   }
 });
