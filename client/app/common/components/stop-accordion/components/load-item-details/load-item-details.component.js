21e41fe3f575c5a0562aa8be7737bacb66dd956c
Patrick Powers <ppowers@echo.com>
2016-08-18 17:15:04 -0500

    component-ize stop details

---
+++ client/app/common/components/stop-accordion/components/load-item-details/load-item-details.component.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('echo.components.loadItemDetails', [
+]).component('loadItemDetails', {
+  bindings: {},
+  templateUrl: 'app/common/components/stop-accordion/components/load-item-details/load-item-details.template.html',
+  controller: function() { 
+  }
+});
--- client/app/common/components/stop-accordion/stop-accordion.component.js
+++ client/app/common/components/stop-accordion/stop-accordion.component.js
@@ -1,7 +1,11 @@
 'use strict';
 
 angular.module('echo.components.stopAccordion', [
-  'echo.components.itemList'
+  'echo.components.itemList',
+  'echo.components.detailsLocation',
+  'echo.components.detailsContact',
+  'echo.components.detailsNotes',
+  'echo.components.loadItemDetails'
 ]).component('stopAccordion', {
   bindings: {},
   templateUrl: 'app/common/components/stop-accordion/stop-accordion.template.html',
