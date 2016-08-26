adeded4bf479610ad9620ac0f6e513b36e4a45b2
Milan Preradovic <milanp@slalom.com>
2016-08-26 13:29:22 -0500

    Change url for staging

--- config/api-config-stage.js
+++ config/api-config-stage.js
@@ -3,6 +3,6 @@ var endpoints = require('./endpoints'),
   _ = require('lodash');
 
 module.exports = function (grunt) {
-  var host = 'http://carr-prtl-stage.dev.echogl.net:81';
+  var host = 'http://carrierportal-stage.dev.echogl.net:81';
   return _.zipObject(_.keys(endpoints), _.map(endpoints, function (value) { return host + value }));
 };
