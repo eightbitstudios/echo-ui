31c4a11a9881245c6916a058e2091c82bf563347
Milan Preradovic <milanp@slalom.com>
2016-08-29 11:32:40 -0500

    Cleaned up app.js

--- client/app/app.js
+++ client/app/app.js
@@ -7,11 +7,8 @@ angular.module('echo', [
   'ngAnimate',
   'base64',
   'ui.mask',
-  'echo.config.api',
-  'echo.config.errors',
-  'echo.services.cookie',
-  'echo.api.authentication',
-  'echo.services.user',
+  'echo.services.routing',
+  'echo.interceptors.auth',
   'ui.bootstrap'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
@@ -19,75 +16,13 @@ angular.module('echo', [
     $locationProvider.html5Mode(false);
     $httpProvider.interceptors.push('authInterceptor');
   })
-
-  .factory('authInterceptor', function ($rootScope, $injector, $window, $q, errorsConfig, routesConfig, cookieService, apiConfig) {
-    return {
-      // Add authorization token to headers
-      request: function (config) {
-        config.headers = config.headers || {};
-        var tokenCookie = cookieService.getToken();
-        var refreshCookie = cookieService.getRefreshToken();
-
-        if (refreshCookie && config.url === apiConfig.refresh) {
-          config.headers.Authorization = 'Bearer ' + refreshCookie;
-        } else if (tokenCookie && config.url !== apiConfig.signIn) {
-          config.headers.Authorization = 'Bearer ' + tokenCookie;
-        }
-
-        return config;
-      },
-      responseError: function (rejection) {
-        var refreshCookie = cookieService.getRefreshToken();
-        if (_.get(rejection, 'data.status.code') === errorsConfig.UNAUTHORIZED && refreshCookie) {
-
-          $rootScope.showLoading = true;
-          var authenticationApi = $injector.get('authenticationApi'); // Avoid circular dependency
-          authenticationApi.refresh().then(function () {
-            $window.location.reload();
-          }).catch(function() {  
-            cookieService.clearToken();
-            cookieService.clearRefreshToken(); 
-            $window.location = routesConfig.LOGIN.base.url({ redirect: encodeURIComponent($window.location.hash) });
-          }).finally(function() {
-            $rootScope.showLoading = false;
-          });
-        }
-        return $q.reject(rejection);
-      }
-    };
-  })
   .controller('AppCtrl', function () { })
-  .run(function ($rootScope, $uibModalStack, $window, $state, userService, routesConfig, cookieService) {
-    $rootScope.$state = $state;
+  .run(function ($rootScope, $uibModalStack, $state, routingService) {
+    $rootScope.$state = $state; //Expose $state to rootScope
+
     // Redirect to login if route requires auth and you're not logged in
     $rootScope.$on('$stateChangeStart', function (event, toState, toParams, from) {//jshint unused:false
-
-      if (_.get(toState.data, 'auth')) { // Check if state requires authentication
-        var jwt = cookieService.getToken();
-        if (from.name !== toState.name) {
-          $state.previous = from;
-        }
-        if (jwt) {  // Check if user is authenticated
-          var user = userService.mapJwtToUser(jwt);
-          if (toState.name === routesConfig.INDEX.base.name) { // Reroute user to their dashboard based on role
-            event.preventDefault();
-
-            if (user.isRepAdmin()) {
-              $state.go(routesConfig.INDEX.myCarriers.name);
-            } else {
-              $state.go(routesConfig.INDEX.activeLoads.name, { carrierId: user.carrierId });
-            }
-          } else if (_.get(toState.data, 'role') && toState.data.role !== _.get(user, 'role')) { // Prevent user from going to states they don't 
-            // have permissions to.
-            event.preventDefault();
-          } else {
-            $rootScope.showLoading = true;
-          }
-        } else {
-          event.preventDefault(); // Reroute user to the login page if they are not authenticated
-          $window.location = routesConfig.LOGIN.base.url({ redirect: encodeURIComponent($window.location.hash) });
-        }
-      }
+      routingService.handleRouting(event, toState, from);
     });
 
     $rootScope.$on('$stateChangeSuccess',
---
+++ client/app/common/services/routing/routing.service.js
@@ -0,0 +1,39 @@
+'use strict';
+
+angular.module('echo.services.routing', [
+  'echo.services.cookie',
+  'echo.services.user',
+  'echo.config.routes'
+])
+  .factory('routingService', function ($rootScope, $window, $state, userService, routesConfig, cookieService) {
+    return {
+      handleRouting: function (event, toState, from) {
+        if (_.get(toState.data, 'auth')) { // Check if state requires authentication
+          var jwt = cookieService.getToken();
+          if (from.name !== toState.name) {
+            $state.previous = from;
+          }
+          if (jwt) {  // Check if user is authenticated
+            var user = userService.mapJwtToUser(jwt);
+            if (toState.name === routesConfig.INDEX.base.name) { // Reroute user to their dashboard based on role
+              event.preventDefault();
+
+              if (user.isRepAdmin()) {
+                $state.go(routesConfig.INDEX.myCarriers.name);
+              } else {
+                $state.go(routesConfig.INDEX.activeLoads.name, { carrierId: user.carrierId });
+              }
+            } else if (_.get(toState.data, 'role') && toState.data.role !== _.get(user, 'role')) { // Prevent user from going to states they don't 
+              // have permissions to.
+              event.preventDefault();
+            } else {
+              $rootScope.showLoading = true;
+            }
+          } else {
+            event.preventDefault(); // Reroute user to the login page if they are not authenticated
+            $window.location = routesConfig.LOGIN.base.url({ redirect: encodeURIComponent($window.location.hash) });
+          }
+        }
+      }
+    };
+  });
