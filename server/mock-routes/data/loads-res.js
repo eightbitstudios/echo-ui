566c42f4291b528ced1b81b2cb261e5f33565c55
Milan Preradovic <milanp@slalom.com>
2016-09-06 14:14:49 -0500

    Bat-169: added first and last name to action component

--- server/mock-routes/data/loads-res.js
+++ server/mock-routes/data/loads-res.js
@@ -80,6 +80,15 @@ module.exports = {
       "isTrackingEnabled": false,
       "isDoNotDisturb": true
     },
+    action: {
+      lastAction: 0,
+      firstName: 'Tim',
+      lastName: 'Potatoe',
+      actionPerformed: '13:50 CST Today',
+      nextAction: 1,
+      onTime: false,
+      lateBy: null
+    },
     "isDetailsUpdated": false,
     "assignedByEcho": false,
     "generalInstructions": "Test Load Detail",
@@ -136,11 +145,9 @@ module.exports = {
       },
       action: {
         lastAction: 0,
-        actionByUser: {
-          firstName: 'Tim',
-          lastName: 'Potatoe'
-        },
-        time: '2016-08-04T16:20:14-05:00',
+        firstName: 'Tim',
+        lastName: 'Potatoe',
+        actionPerformed: '13:50 CST Today',
         nextAction: 2,
         onTime: false,
         lateBy: null
@@ -172,11 +179,9 @@ module.exports = {
       driver: {},
       action: {
         lastAction: 0,
-        actionByUser: {
-          firstName: 'Tim',
-          lastName: 'Potatoe'
-        },
-        time: '2016-08-04T16:20:14-05:00',
+        firstName: 'Tim',
+        lastName: 'Potatoe',
+        actionPerformed: '13:50 CST Today',
         nextAction: 1,
         onTime: false,
         lateBy: null
@@ -209,12 +214,11 @@ module.exports = {
       },
       action: {
         lastAction: 0,
-        actionByUser: {
-          firstName: 'Mike',
-          lastName: 'Potatoe'
-        },
+        firstName: 'Tim',
+        lastName: 'Potatoe',
         time: '1473417000',
         nextAction: 1,
+        actionPerformed: '13:50 CST Today',
         onTime: true,
         lateBy: null
       },
@@ -246,11 +250,9 @@ module.exports = {
       },
       action: {
         lastAction: 0,
-        actionByUser: {
-          firstName: 'Tim',
-          lastName: 'Potatoe'
-        },
-        time: '2016-08-04T16:20:14-05:00',
+        firstName: 'Tim',
+        lastName: 'Potatoe',
+        actionPerformed: '13:50 CST Today',
         nextAction: 1,
         onTime: false,
         lateBy: null
@@ -282,11 +284,9 @@ module.exports = {
       driver: {},
       action: {
         lastAction: 1,
-        actionByUser: {
-          firstName: 'Tim',
-          lastName: 'Potatoe'
-        },
-        time: '2016-08-04T16:20:14-05:00',
+        firstName: 'Tim',
+        lastName: 'Potatoe',
+        actionPerformed: '13:50 CST Today',
         nextAction: 2,
         onTime: false,
         lateBy: null
@@ -319,11 +319,9 @@ module.exports = {
       },
       action: {
         lastAction: 0,
-        actionByUser: {
-          firstName: 'Mike',
-          lastName: 'Potatoe'
-        },
-        time: '1473417000',
+        firstName: 'Mike',
+        lastName: 'Potatoe',
+        actionPerformed: '13:50 CST Today',
         nextAction: 1,
         onTime: true,
         lateBy: null
@@ -356,11 +354,9 @@ module.exports = {
       },
       action: {
         lastAction: 0,
-        actionByUser: {
-          firstName: 'Tim',
-          lastName: 'Potatoe'
-        },
-        time: '2016-08-04T16:20:14-05:00',
+        firstName: 'Tim',
+        lastName: 'Potatoe',
+        actionPerformed: '13:50 CST Today',
         nextAction: 1,
         onTime: false,
         lateBy: null
@@ -392,11 +388,9 @@ module.exports = {
       driver: {},
       action: {
         lastAction: 0,
-        actionByUser: {
-          firstName: 'Tim',
-          lastName: 'Potatoe'
-        },
-        time: '2016-08-04T16:20:14-05:00',
+        firstName: 'Tim',
+        lastName: 'Potatoe',
+        actionPerformed: '13:50 CST Today',
         nextAction: 1,
         onTime: false,
         lateBy: null
@@ -428,11 +422,9 @@ module.exports = {
       driver: {},
       action: {
         lastAction: 0,
-        actionByUser: {
-          firstName: 'Tim',
-          lastName: 'Potatoe'
-        },
-        time: '2016-08-04T16:20:14-05:00',
+        firstName: 'Tim',
+        lastName: 'Potatoe',
+        actionPerformed: '13:50 CST Today',
         nextAction: 1,
         onTime: false,
         lateBy: null
@@ -464,11 +456,9 @@ module.exports = {
       driver: {},
       action: {
         lastAction: 0,
-        actionByUser: {
-          firstName: 'Tim',
-          lastName: 'Potatoe'
-        },
-        time: '2016-08-04T16:20:14-05:00',
+        firstName: 'Tim',
+        lastName: 'Potatoe',
+        actionPerformed: '13:50 CST Today',
         nextAction: 1,
         onTime: false,
         lateBy: null
@@ -500,11 +490,9 @@ module.exports = {
       driver: {},
       action: {
         lastAction: 0,
-        actionByUser: {
-          firstName: 'Tim',
-          lastName: 'Potatoe'
-        },
-        time: '2016-08-04T16:20:14-05:00',
+        firstName: 'Tim',
+        lastName: 'Potatoe',
+        actionPerformed: '13:50 CST Today',
         nextAction: 1,
         onTime: false,
         lateBy: null
