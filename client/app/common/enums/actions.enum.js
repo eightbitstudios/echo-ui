0ad0c6b6b7c7bf6a53d1ffea6129e25532eb1c43
Milan Preradovic <milanp@slalom.com>
2016-09-07 14:10:20 -0500

    BAT-169: Changed Start to booked

--- client/app/common/enums/actions.enum.js
+++ client/app/common/enums/actions.enum.js
@@ -1,7 +1,7 @@
 angular.module('echo.enums.actions', [])
   .constant('actionEnums', {
-    REPORTED_START: {
-      description: 'Reported Start',
+    REPORTED_BOOKED: {
+      description: 'Reported Booked',
       value: 0
     },
     REPORTED_EMPTY: {
