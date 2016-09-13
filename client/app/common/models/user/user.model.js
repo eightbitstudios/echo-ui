33b46d5e6a250c6fd69f4a3f2def5220760b0d03
Milan Preradovic <milanp@slalom.com>
2016-09-13 14:59:45 -0500

    Fixed error when adding a new user

--- client/app/common/models/user/user.model.js
+++ client/app/common/models/user/user.model.js
@@ -14,7 +14,7 @@ angular.module('echo.models.user', [
       var that = this;
 
       _.assign(that, userData);
-      that.roleName = userData.oneLoginRoleName;
+      that.roleName = _.get(userData, 'oneLoginRoleName');
     }
 
     /**
