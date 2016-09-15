e47c4cddb6dbe21aad6e914657d7f889dcd44e4b
Andy Kilinskis <andyk@slalom.com>
2016-09-15 16:23:15 -0500

    Unit tests to cover gaps in filter class testing, plus some loads API tests

---
+++ client/app/common/filters/date-picker/date-picker.filter.spec.js
@@ -0,0 +1,25 @@
+'use strict';
+
+describe('Filter - Date picker filter', function () {
+  var datePicker;
+
+  beforeEach(function () {
+    module('echo.filters.datePicker');
+
+    inject(function ($filter) {
+      datePicker = $filter('datePicker');
+    });
+  });
+
+  it('should return Tue, Sep 20', function () {
+    expect(datePicker('2016-09-20T12:00:00')).toEqual('Tue, Sep 20');
+  });
+
+  it('should return Tue, Jul 04', function () {
+    expect(datePicker('2017-07-04T12:00:00')).toEqual('Tue, Jul 04');
+  });
+
+  it('should return Thu, Jan 01', function () {
+    expect(datePicker('2015-01-01T12:00:00')).toEqual('Thu, Jan 01');
+  });
+});
