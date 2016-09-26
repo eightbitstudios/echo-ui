67ef5b0721d3926858c1ef66f3783036707bb986
Milan Preradovic <milanp@slalom.com>
2016-09-26 15:51:46 -0500

    Removed duplicate code

--- client/app/common/components/modal/assign-driver-modal/assign-driver-modal.component.js
+++ client/app/common/components/modal/assign-driver-modal/assign-driver-modal.component.js
@@ -62,8 +62,7 @@ angular.module('echo.components.modal.assignDriver', [
         that.errorMessages = null;
         that.errorCode = null;
         loadsApi.unassignDriver(that.load.loadNumber).then(function () {
-          that.driverChanged = true;          
-          that.driverChanged = true;
+          that.driverChanged = true;   
           that.modalActions.close(that.driverChanged);
         }).catch(function (status) {
           that.errorMessages = status.message;
