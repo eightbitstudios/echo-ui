10ce563fb142dfc994eb7ffcafb636a65c609764
Andy Kilinskis <andyk@slalom.com>
2016-09-27 17:20:55 -0500

    BAT-1715: Moved Pickup Numbers to Accordion Component, stacked if more than one per load

--- client/app/pages/index/carrier/components/load-management/components/load-details/components/stop-accordion/stop-accordion.component.js
+++ client/app/pages/index/carrier/components/load-management/components/load-details/components/stop-accordion/stop-accordion.component.js
@@ -3,13 +3,15 @@
 angular.module('echo.components.stopAccordion', [
   'echo.components.itemList',
   'echo.components.detailsLocation',
+  'echo.components.detailsPickupNumbers',
   'echo.components.detailsContact',
   'echo.components.detailsNotes',
   'echo.components.loadItemDetails'
 ]).component('stopAccordion', {
   bindings: {
     stop: '<',
-    stopType: '@'
+    stopType: '@',
+    pickupNumbers: '<'
   },
   templateUrl: 'app/pages/index/carrier/components/load-management/components/load-details/components/stop-accordion/stop-accordion.template.html',
   controller: function () {
