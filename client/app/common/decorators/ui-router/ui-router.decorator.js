84fc0f55b35b2fae2f242d7b38e568027134ccb9
Stephen Weinrich <stephenw@slalom.com>
2016-09-27 17:24:52 -0500

    Revert "Merge branch 'tech-debt/UI-router-resolve-test' into 'develop'"
    This reverts commit c93b3a9cda1905d692e785bcd92b8b2825d64b72, reversing
    changes made to a8b192196f87a7f67a58590d26dd08554db51d7f.

--- client/app/common/decorators/ui-router/ui-router.decorator.js
+++ client/app/common/decorators/ui-router/ui-router.decorator.js
@@ -1,11 +1,45 @@
-angular.module('echo.decorators.uiRouter', [
-  'ui.router',
-  'echo.decorators.uiRouter.component'
-])
-  .config(function ($stateProvider, componentDecoratorProvider) {
-    var componentDecorator = componentDecoratorProvider.$get();
+angular.module('echo.decorators.uiRouter', [])
+  .config(function ($stateProvider) {
     /**
      * Custom component decorator for ui-router
      */
-    $stateProvider.decorator('component', componentDecorator.buildStateView);
+    $stateProvider.decorator('component', function (state) {
+      if (state.self.component) {
+
+        state.self.controllerAs = '$ctrl';
+        
+        /**
+         * Grabs all resolve property keys from the state object.
+         * example - ['carrierId', 'portalUsers']
+         */
+        var resolve = _(state.path).map(function (value) {
+          return _.keys(value.resolve);
+        }).flatten().value();
+
+        /**
+         * Builds out a template for a component
+         * @example <portal-users user="$ctrl.users"/>
+         */
+        state.self.template = _.template('<${component} ${bindings}><${component}/>')({
+          component: state.self.component,
+          bindings: _(resolve).map(function (value) {
+            return _.template('${bindingName}="${ctrlName}.${binding}"')({
+              bindingName: _.kebabCase(value),
+              binding: value,
+              ctrlName: state.self.controllerAs
+            });
+          }).join(' ') || ''
+        });
+
+        /**
+         * Control for component template. This allows the component to have access to all of the resolve dependencies
+         */
+        state.self.controller = _.rest(function (dependencies) {
+          _.assign(this, _.zipObject(resolve, dependencies));
+        });
+
+        // Inject all resolve dependencies
+        state.self.controller.$inject = resolve;
+      }
+    });
   });
