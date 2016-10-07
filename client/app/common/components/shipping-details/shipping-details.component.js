a2d565b3a419099b66edfef5f1d9b40153d36740
Andy Kilinskis <andyk@slalom.com>
2016-10-07 09:04:48 -0500

    BAT-1863: Conditionally gray out pickup/delivery on loads depending on next action

--- client/app/common/components/shipping-details/shipping-details.component.js
+++ client/app/common/components/shipping-details/shipping-details.component.js
@@ -5,12 +5,13 @@ angular.module('echo.components.shippingDetails', [
   .component('shippingDetails', {
     templateUrl: 'app/common/components/shipping-details/shipping-details.template.html',
     bindings: {
-      shippingDetails: '<'
+      shippingDetails: '<',
+      inactive: '<'
     },
     controller: function (PagingModel) {
       var that = this;
       that.paging = new PagingModel(1);
-      
+
       if (_.isArray(that.shippingDetails)) {
         that.location = _.find(that.shippingDetails, { isCurrent: true }) ||  _.last(that.shippingDetails);
         that.paging.selectedPage = _.findIndex(that.shippingDetails, that.location) + 1;
