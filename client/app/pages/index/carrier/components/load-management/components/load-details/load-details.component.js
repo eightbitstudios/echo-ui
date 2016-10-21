de88ce79e91c94dbb53eb7e12b968efbd6543c90
Andy Kilinskis <andyk@slalom.com>
2016-10-21 11:12:24 -0500

    [Tech Debt] Render Load Details page with header and footer remaining on page

--- client/app/pages/index/carrier/components/load-management/components/load-details/load-details.component.js
+++ client/app/pages/index/carrier/components/load-management/components/load-details/load-details.component.js
@@ -5,23 +5,30 @@ angular.module('echo.index.carrier.loadManagement.loadDetails', [
   'echo.components.mapPlaceholder',
   'echo.index.carrier.loadManagement.loadDetails.loadDetail',
   'echo.index.carrier.loadManagement.loadDetails.documents',
-  'echo.index.carrier.loadManagement.loadDetails.activityLog'
+  'echo.index.carrier.loadManagement.loadDetails.activityLog',
+  'echo.api.loads'
 ])
   .component('loadDetails', {
     templateUrl: 'app/pages/index/carrier/components/load-management/components/load-details/load-details.template.html',
     bindings: {
       repDetails: '<',
       carrierId: '<',
-      loadDetails: '<',
-      activityLog: '<'
+      loadId: '<'
     },
-    controller: function ($state) {
+    controller: function ($state, $q, loadsApi) {
       var that = this;
 
-      that.pickupNumbers = _.map(that.loadDetails.pickUp, 'pickupNumber');
-      that.deliveryNumbers = _.map(that.loadDetails.delivery, 'pickupNumber');
-
       that.$onInit = function () {
+        that.showLoading = true;
+        $q.all([loadsApi.fetchLoadDetails(that.loadId), loadsApi.fetchActivityLogByLoadId(that.loadId)])
+          .then(_.spread(function (loadDetails, activityLog) {
+            that.loadDetails = loadDetails;
+            that.activityLog = activityLog;
+            that.pickupNumbers = _.map(that.loadDetails.pickUp, 'pickupNumber');
+            that.deliveryNumbers = _.map(that.loadDetails.delivery, 'pickupNumber');
+            that.showLoading = false;
+          }));
+
         if ($state.previous.data) {
           that.previousRouteName = $state.previous.data.name;
           that.previousRoute = $state.previous.name;
