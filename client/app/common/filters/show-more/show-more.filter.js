4181fdd9a3fbb1edb4e593d8b35221f9ee91821e
Milan Preradovic <milanp@slalom.com>
2016-11-11 10:38:20 -0600

    Fix dashboard show more count

--- client/app/common/filters/show-more/show-more.filter.js
+++ client/app/common/filters/show-more/show-more.filter.js
@@ -5,7 +5,7 @@ angular.module('echo.filters.showMore', [])
     return function (paging) {
       var showMore = '';
 
-      var count = (paging.totalRecords - (paging.offset + paging.limit));
+      var count =  (paging.totalRecords - (paging.offset + paging.limit) + 1);
 
       if (count > 0) {
         showMore = _.template('Show ${count} more Loads')({
