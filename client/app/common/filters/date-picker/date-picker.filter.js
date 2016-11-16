4eb4b06ca4f497f3b8edc6fc34c8fd222be5f613
Milan Preradovic <milanp@slalom.com>
2016-11-16 10:54:58 -0600

    Revert date picker filter

--- client/app/common/filters/date-picker/date-picker.filter.js
+++ client/app/common/filters/date-picker/date-picker.filter.js
@@ -4,14 +4,8 @@ angular.module('echo.filters.datePicker', [
   'echo.config.globals'
 ])
   .filter('datePicker', function (moment) {
-    return function (date, minDate) {
-      if(date) {
-        return moment(date).format('ddd, MMM DD');
-      } else if(minDate) {
-        return moment(minDate, 'MM/DD/YYYY HH:mm:ss').format('ddd, MMM DD');
-      } else {
-        return moment();
-      }
-      
+    return function (date) {
+      return moment(date).format('ddd, MMM DD');
     };
   });
+
