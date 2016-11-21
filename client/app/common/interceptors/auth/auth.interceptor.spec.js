5beaf1333eb2adafe88d1602d6a6e5895c4de414
Milan Preradovic <milanp@slalom.com>
2016-11-21 12:05:02 -0600

    Fix authentication refresh and redirect

--- client/app/common/interceptors/auth/auth.interceptor.spec.js
+++ client/app/common/interceptors/auth/auth.interceptor.spec.js
@@ -1,4 +1,4 @@
-describe('Interceptor: authInterceptor', function () {
+describe('Interceptor: authInterceptor', function() {
   'use strict';
 
   var $scope,
@@ -11,9 +11,11 @@ describe('Interceptor: authInterceptor', function () {
     authenticationApi,
     authInterceptor;
 
-  beforeEach(function () {
-    module('echo.interceptors.auth', function ($provide) {
-      $provide.value('$window', $window = { location: '' });
+  beforeEach(function() {
+    module('echo.interceptors.auth', function($provide) {
+      $provide.value('$window', $window = {
+        location: ''
+      });
 
       $provide.value('cookieService', cookieService = jasmine.createSpyObj('cookieService', ['getToken', 'getRefreshToken', 'clearToken', 'clearRefreshToken']));
       $provide.value('authenticationApi', authenticationApi = jasmine.createSpyObj('authenticationApi', ['refresh']));
@@ -40,16 +42,16 @@ describe('Interceptor: authInterceptor', function () {
       });
     });
 
-    inject(function ($rootScope, _$q_, _authInterceptor_) {
+    inject(function($rootScope, _$q_, _authInterceptor_) {
       $scope = $rootScope.$new();
       $q = _$q_;
       authInterceptor = _authInterceptor_;
     });
   });
 
-  describe('Function: request', function () {
+  describe('Function: request', function() {
     var config;
-    beforeEach(function () {
+    beforeEach(function() {
       config = {
         url: ''
       };
@@ -58,76 +60,74 @@ describe('Interceptor: authInterceptor', function () {
       cookieService.getRefreshToken.and.returnValue('refresh');
     });
 
-    it('should send refresh token when calling the refresh endpoint', function () {
+    it('should send refresh token when calling the refresh endpoint', function() {
       config.url = apiConfig.refresh;
       expect(authInterceptor.request(config).headers.Authorization).toEqual('Bearer refresh');
     });
 
-    it('should send auth token when calling the authorized endpoints', function () {
+    it('should send auth token when calling the authorized endpoints', function() {
       config.url = apiConfig.other;
       expect(authInterceptor.request(config).headers.Authorization).toEqual('Bearer token');
     });
 
-    it('should not send token if calling sign in endpoint', function () {
+    it('should not send token if calling sign in endpoint', function() {
       config.url = apiConfig.signIn;
       expect(authInterceptor.request(config).headers.Authorization).toBeUndefined();
     });
   });
 
-  describe('Function: responseError', function () {
+  describe('Function: responseError', function() {
     var rejection;
 
-    beforeEach(function () {
+    beforeEach(function() {
       rejection = {
         data: {
-          status: {
-            code: ''
-          }
+          code: ''
         }
       };
       cookieService.getRefreshToken.and.returnValue('refresh');
     });
 
-    it('should bypass refresh token if status code is not 401', function () {
+    it('should bypass refresh token if status code is not 401', function() {
       authInterceptor.responseError(rejection);
       expect(authenticationApi.refresh).not.toHaveBeenCalled();
     });
 
-    describe('refresh token', function () {
+    describe('refresh token', function() {
       var refreshDefer;
-      beforeEach(function () {
+      beforeEach(function() {
         refreshDefer = $q.defer();
         authenticationApi.refresh.and.returnValue(refreshDefer.promise);
-        rejection.data.status.code = errorsConfig.UNAUTHORIZED;
+        rejection.data.code = errorsConfig.UNAUTHORIZED;
         $window.location = jasmine.createSpyObj('location', ['reload']);
       });
 
-      it('should call refresh token endpoint', function () {
+      it('should call refresh token endpoint', function() {
         authInterceptor.responseError(rejection);
         expect(authenticationApi.refresh).toHaveBeenCalled();
       });
 
-      it('should reload page if successful', function (done) {
+      it('should reload page if successful', function(done) {
         refreshDefer.resolve();
         authInterceptor.responseError(rejection);
-        refreshDefer.promise.then(function () {
+        refreshDefer.promise.then(function() {
           expect($window.location.reload).toHaveBeenCalled();
           done();
         });
         $scope.$digest();
       });
 
-      it('should redirect to login page if refresh token failed', function (done) {
+      it('should redirect to login page if refresh token failed', function(done) {
         refreshDefer.reject();
         authInterceptor.responseError(rejection);
         $scope.$digest();
-        refreshDefer.promise.catch(function () {
+        refreshDefer.promise.catch(function() {
           expect(cookieService.clearToken).toHaveBeenCalled();
           expect(cookieService.clearRefreshToken).toHaveBeenCalled();
           expect(routesConfig.LOGIN.base.url).toHaveBeenCalled();
           done();
         });
-        
+
         $scope.$digest();
       });
     });
