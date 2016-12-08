1fb82a765e4a72aa4f90e139332e783946f06cc9
Milan Preradovic <milanp@slalom.com>
2016-12-08 16:23:14 -0600

    Added pagination component

--- client/app/common/components/modal/document-overview-modal/components/document-preview/document-preview.component.js
+++ client/app/common/components/modal/document-overview-modal/components/document-preview/document-preview.component.js
@@ -1,9 +1,22 @@
 angular.module('echo.components.modal.documentOverview.documentPreview', [
-  'echo.filters.fullName'
-])
+    'echo.filters.fullName',
+    'echo.models.paging',
+    'echo.components.pagination'
+  ])
   .component('documentPreview', {
     templateUrl: 'app/common/components/modal/document-overview-modal/components/document-preview/document-preview.template.html',
     bindings: {
       document: '<'
+    },
+    controller: function(PagingModel) {
+      var that = this;
+      that.paging = new PagingModel(1);
+
+      that.$onChanges = function(changeObj) {
+        if (changeObj.document.currentValue) {
+          that.paging.reset();
+          that.paging.setRecords(_.size(changeObj.document.currentValue.loadDocumentPages), 1);
+        }
+      };
     }
   });
