91240bb6c28b0d13815b716abd282e13a59b03c0
Andy Kilinskis <andyk@slalom.com>
2016-12-20 14:54:31 -0600

    [Feature] BAT-2109: Change Terms & Conditions page into a modal

--- client/app/pages/index/terms-and-conditions/terms-and-conditions.component.js
+++ client/app/pages/index/terms-and-conditions/terms-and-conditions.component.js
@@ -1,5 +1,8 @@
 angular.module('echo.index.termsAndConditions', [
 ]).component('termsAndConditions', {
   templateUrl: 'app/pages/index/terms-and-conditions/terms-and-conditions.template.html',
+  bindings: {
+    modalActions: '<'
+  },
   controller: function () {}
 });
