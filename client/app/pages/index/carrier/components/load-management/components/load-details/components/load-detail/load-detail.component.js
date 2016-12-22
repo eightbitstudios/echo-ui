837a4e598241e4fdf36aa67505e0454a2cc5c3c6
Andy Kilinskis <andyk@slalom.com>
2016-12-22 14:31:16 -0600

    Add dynamic highlighting to load details page

--- client/app/pages/index/carrier/components/load-management/components/load-details/components/load-detail/load-detail.component.js
+++ client/app/pages/index/carrier/components/load-management/components/load-details/components/load-detail/load-detail.component.js
@@ -1,6 +1,7 @@
 angular.module('echo.index.carrier.loadManagement.loadDetails.loadDetail', [
   'echo.index.carrier.loadManagement.loadDetails.loadDetail.proNumber',
-  'echo.enums.loadTypes'
+  'echo.enums.loadTypes',
+  'echo.enums.actions'
 ])
   .component('loadDetail', {
     templateUrl: 'app/pages/index/carrier/components/load-management/components/load-details/components/load-detail/load-detail.template.html',
@@ -9,13 +10,25 @@ angular.module('echo.index.carrier.loadManagement.loadDetails.loadDetail', [
       carrierId: '<',
       repDetails: '<'
     },
-    controller: function ($state, loadTypesEnum) {
+    controller: function ($state, loadTypesEnum, actionEnums) {
       var that = this;
       that.loadType = loadTypesEnum.ACTIVE;
       that.reloadState = function () {
         $state.reload();
       };
-      
+
       that.isMultiStop = _.size(that.loadDetail.pickUp) > 1 || _.size(that.loadDetail.delivery) > 1;
+
+      that.determineInactive = function(nextAction, isPickup) {
+        if (!nextAction) {
+          return false;
+        }
+        var action = _.find(actionEnums.AVAILABLE_ACTIONS, function (action) { return action.value === nextAction; });
+        if (isPickup) {
+          return action.phase === 2 || action.phase === 3;
+        } else {
+          return action.phase === 1 || action.phase === 3;
+        }
+      };
     }
   });
