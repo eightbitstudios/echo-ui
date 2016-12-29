42c46b4abd72b08b1eab3a4b2093ad8e5f62afdb
Milan Preradovic <milanp@slalom.com>
2016-12-29 14:35:55 -0600

    Add v1 to azure api

--- config/api-config-stage.js
+++ config/api-config-stage.js
@@ -3,7 +3,7 @@ var endpoints = require('./endpoints'),
   _ = require('lodash');
 
 module.exports = function (grunt) {
-  var host = 'http://echotestapi.azure-api.net/web-api';
+  var host = 'http://echotestapi.azure-api.net/web-api/v1';
   var apiEndpoints = _.zipObject(_.keys(endpoints), _.map(endpoints, function (value) { return host + value }));
   apiEndpoints.googleMapsApiKey = 'AIzaSyC8RsoHfRqThfEIun95B9Q57aN2BT8L_OI';
   apiEndpoints.key = new Buffer('bd045befc19a40a68840b2d16b6dd465').toString('base64');
