3ee3f5c6bcb944b8f0f9254eebf30cefd1ef2de6
Andy Kilinskis <andyk@slalom.com>
2017-01-09 10:19:23 -0600

    Refactored usage component to utilize instanceof on portal user and driver model object types

--- client/app/common/components/usage/usage.component.js
+++ client/app/common/components/usage/usage.component.js
@@ -1,19 +1,27 @@
 'use strict';
 
 angular.module('echo.components.usage', [
-  'echo.components.resendInvite'
+  'echo.components.resendInvite',
+  'echo.models.user',
+  'echo.models.driver'
 ]).component('usage', {
   bindings: {
-    user: '<',
-    isPortalUser: '<',
-    isDriver: '<'
+    user: '<'
   },
   templateUrl: 'app/common/components/usage/usage.template.html',
-  controller: function () {
+  controller: function (UserModel, DriverModel) {
     var that = this;
 
     that.hasUserLoggedIn = function () {
       return that.user.lastLogin !== 'Never';
+    };
+
+    that.isPortalUser = function () {
+      return that.user instanceof UserModel;
+    };
+
+    that.isDriver = function () {
+      return that.user instanceof DriverModel;
     };
   }
 });
