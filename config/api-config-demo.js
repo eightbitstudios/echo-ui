ca32f3daf08e97882338a4a029851902f851c9fe
Andy Kilinskis <andyk@slalom.com>
2017-01-12 11:06:52 -0600

    Replace Google Maps API Keys with a neutral Google account's key, test out Echo API Key in dev

--- config/api-config-demo.js
+++ config/api-config-demo.js
@@ -5,7 +5,7 @@ var endpoints = require('./endpoints'),
 module.exports = function (grunt) {
   var host = 'http://carr-prtl.dev.echogl.net:81/api/v1';
   var apiEndpoints = _.zipObject(_.keys(endpoints), _.map(endpoints, function (value) { return host + value }));
-  apiEndpoints.googleMapsApiKey = 'AIzaSyC8RsoHfRqThfEIun95B9Q57aN2BT8L_OI';
+  apiEndpoints.googleMapsApiKey = 'AIzaSyCVP3yvKSvxHj2dWHfKCVClFR0wz0vgutw';
   apiEndpoints.key = '';
   apiEndpoints.keyHeader = '';
   return apiEndpoints;
