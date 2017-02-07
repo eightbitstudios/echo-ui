d2d24cc155b3f0e82a71b49e122bb6e3bbae1c72
Milan Preradovic <milanp@slalom.com>
2017-02-07 14:12:53 -0600

    Added default version

--- tasks/jenkins-version.js
+++ tasks/jenkins-version.js
@@ -1,5 +1,8 @@
 module.exports = function(grunt) {
-  var version = {};
+  var version = {
+    buildVersion: '0.0.0'
+  };
+  
   if (grunt.file.exists('version.json')) {
     version = grunt.file.readJSON('version.json');
   }
