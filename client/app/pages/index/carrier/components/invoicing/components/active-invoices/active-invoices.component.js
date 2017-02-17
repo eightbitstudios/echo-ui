8a65becb451ab8d77683cf9a2e2e93ce64138125
Andy Kilinskis <andyk@slalom.com>
2017-02-17 10:28:35 -0600

    [Feature] BAT-164 (among others): Filtering for Active Invoices

--- client/app/pages/index/carrier/components/invoicing/components/active-invoices/active-invoices.component.js
+++ client/app/pages/index/carrier/components/invoicing/components/active-invoices/active-invoices.component.js
@@ -1,6 +1,8 @@
 angular.module('echo.index.carrier.invoicing.activeInvoices', [
   'echo.index.carrier.invoicing.invoiceTable',
+  'echo.index.carrier.invoicing.invoicesFilter',
   'echo.models.paging',
+  'echo.enums.invoices',
   'echo.config.appConstants',
   'echo.api.invoices',
   'echo.action',
@@ -9,12 +11,27 @@ angular.module('echo.index.carrier.invoicing.activeInvoices', [
   .component('activeInvoices', {
     templateUrl: 'app/pages/index/carrier/components/invoicing/components/active-invoices/active-invoices.template.html',
     bindings: {},
-    controller: function (PagingModel, appConstants, invoicesApi, store$, invoiceCountsActionCreator, routesConfig) {
-      this.fetchActiveInvoices = function () {
+    controller: function (PagingModel, appConstants, invoicesApi, store$, invoiceCountsActionCreator, routesConfig, invoiceEnums) {
+      var that = this;
+
+      that.filterHandler = function(value, enumValue) {
+        var statusEnum = _.find(invoiceEnums.STATUSES, function(status) { return status.value === enumValue; });
+        if (!value || !statusEnum) {
+          that.filterText = that.defaultFilterText;
+          delete that.filterBy;
+        } else {
+          that.filterText = 'By ' + statusEnum.description;
+          that.filterBy = enumValue;
+        }
+        that.paging.reset();
+        that.fetchActiveInvoices();
+      };
+
+      that.fetchActiveInvoices = function () {
         var that = this;
         that.showLoading = true;
 
-        invoicesApi.fetchActiveInvoices(that.carrierId, that.paging).then(function(invoicesPageData) {
+        invoicesApi.fetchActiveInvoices(that.carrierId, that.paging, that.filterBy).then(function(invoicesPageData) {
           if (invoicesPageData.invoices) {
             that.activeInvoices = invoicesPageData.invoices;
             that.paging.recordCount = _.size(invoicesPageData.invoices);
@@ -32,14 +49,17 @@ angular.module('echo.index.carrier.invoicing.activeInvoices', [
         });
       };
 
-      this.$onInit = function() {
+      that.$onInit = function() {
         var state = store$.getState();
-        this.carrierId = state.carrier.carrierId;
-        this.repDetails = state.rep;
+        that.carrierId = state.carrier.carrierId;
+        that.repDetails = state.rep;
+        that.defaultFilterText = 'By Received Date';
+        that.filterText = that.defaultFilterText;
+        that.statusEnums = invoiceEnums.STATUSES;
 
-        this.unbilledLoads = routesConfig.INDEX.unbilledLoads.name;
-        this.paging = new PagingModel(appConstants.LIMIT.invoicesList);
-        this.fetchActiveInvoices();
+        that.unbilledLoads = routesConfig.INDEX.unbilledLoads.name;
+        that.paging = new PagingModel(appConstants.LIMIT.invoicesList);
+        that.fetchActiveInvoices();
       };
     }
   });
