54b1e634c3819805431cd958ef3f54808971a7a0
Andy Kilinskis <andyk@slalom.com>
2017-02-20 16:59:07 -0600

    [Feature] BAT-2971: Add Moved to Invoicing link to unbilled loads after document upload

--- client/app/common/components/load-table/components/action/action.component.js
+++ client/app/common/components/load-table/components/action/action.component.js
@@ -14,13 +14,24 @@ angular.module('echo.components.loadTable.action', [
       repDetails: '<',
       isMultiStop: '<'
     },
-    controller: function (actionEnums, loadTypesEnum) {
-      this.$onInit = function () {
-        this.currentStatus = _.find(actionEnums.LAST_ACTION, { value: _.get(this.load.nextAction, 'lastAction') });
-        this.nextAction = _.find(actionEnums.AVAILABLE_ACTIONS, { value: _.get(this.load.nextAction, 'nextAction') });
-        this.isAddDocuments = (this.nextAction === actionEnums.AVAILABLE_ACTIONS.ADD_DOCUMENTS);
-        this.isBooked = (this.currentStatus === actionEnums.LAST_ACTION.BOOKED);
-        this.loadTypesEnum = loadTypesEnum;
+    controller: function (actionEnums, loadTypesEnum, routesConfig) {
+      var that = this;
+
+      that.documentsRequired = function () {
+        return that.load.needsInvoice || that.load.neededPODs > 0;
+      };
+
+      that.disableActionButton = function () {
+        return that.loadType === that.loadTypesEnum.UNBILLED && !that.documentsRequired();
+      };
+
+      that.$onInit = function () {
+        that.currentStatus = _.find(actionEnums.LAST_ACTION, { value: _.get(this.load.nextAction, 'lastAction') });
+        that.nextAction = _.find(actionEnums.AVAILABLE_ACTIONS, { value: _.get(this.load.nextAction, 'nextAction') });
+        that.isAddDocuments = (this.nextAction === actionEnums.AVAILABLE_ACTIONS.ADD_DOCUMENTS);
+        that.isBooked = (this.currentStatus === actionEnums.LAST_ACTION.BOOKED);
+        that.loadTypesEnum = loadTypesEnum;
+        that.routesConfig = routesConfig;
       };
     }
   });
