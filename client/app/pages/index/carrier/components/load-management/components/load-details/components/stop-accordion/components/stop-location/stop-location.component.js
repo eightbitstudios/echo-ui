3b86d481b41626af5b0883a2ea7b4aae3dec8a25
Milan Preradovic <milanp@slalom.com>
2017-02-23 13:50:06 -0600

    BAT-3258: Added renamed pickup numbers to delivery numbers

---
+++ client/app/pages/index/carrier/components/load-management/components/load-details/components/stop-accordion/components/stop-location/stop-location.component.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('echo.components.stopLocation', [
+  'echo.services.googleMaps'
+]).component('stopLocation', {
+  bindings: {
+    stop: '<'
+  },
+  templateUrl: 'app/pages/index/carrier/components/load-management/components/load-details/components/stop-accordion/components/stop-location/stop-location.template.html',
+  controller: function(googleMaps) {
+    this.getMapsUrl = function() {
+      return googleMaps.getMapsUrlByCityState(this.stop.address, this.stop.city, this.stop.state);
+    };
+  }
+});
